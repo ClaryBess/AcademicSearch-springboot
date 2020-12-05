a4cd9709e85825dee0bfc54c3ae905821253f81b
Qiaoyw <63182186+Qiaoyw@users.noreply.github.com>
2020-12-05 19:51:34 +0800

    Delete ArticleSearchRepository.java

--- src/main/java/com/example/demo/ArticleSearchRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import com.example.demo.bean.Article;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
- 
-//泛型的参数分别是实体类型和主键类型
-public interface ArticleSearchRepository extends ElasticsearchRepository<Article, Long>{
- 
-}
