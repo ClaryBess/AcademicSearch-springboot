11539e4d3a19294dbf3036b331139781270fac58
LiShushi <43929922+LiCarry@users.noreply.github.com>
2020-12-10 20:01:35 +0800

    更改User：增加了若干属性； (#2)
    更新UserMapper：提供通过id查询、删除用户以及添加用户
    更新UserService：提供通过id查询、删除用户以及添加用户
    新建UserController：未完成

--- src/main/java/com/example/demo/bean/User.java
+++ src/main/java/com/example/demo/bean/User.java
@@ -1,16 +1,29 @@
 package com.example.demo.bean;
 
 public class User {
-    private Integer id;
+    Integer id;
     String name;
+    String organization;
     String email;
+    Integer paperCount; //论文数目
+    Integer index;  //为n_index指数
     String pwd;
+    String info;
+    Integer role;   //0为管理员，1为普通用户，2为学者
 
-    public User(Integer id, String name, String email, String pwd) {
+    public User(Integer id, String name, String organization, String email, Integer paperCount, Integer index, String pwd, String info, Integer role) {
         this.id = id;
         this.name = name;
+        this.organization = organization;
         this.email = email;
+        this.paperCount = paperCount;
+        this.index = index;
         this.pwd = pwd;
+        this.info = info;
+        this.role = role;
+    }
+
+    public User() {
     }
 
     public Integer getId() {
@@ -43,5 +56,45 @@ public class User {
 
     public void setPwd(String pwd) {
         this.pwd = pwd;
+    }
+
+    public Integer getPaperCount() {
+        return paperCount;
+    }
+
+    public void setPaperCount(Integer paperCount) {
+        this.paperCount = paperCount;
+    }
+
+    public Integer getRole() {
+        return role;
+    }
+
+    public void setRole(Integer role) {
+        this.role = role;
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
     }
 }
