f295c5ac0068628469f93b377a5f6e5b58114c39
李书实 <15510269739@163.com>
2020-12-10 19:59:31 +0800

    更改User：增加了若干属性； 更新UserMapper：提供通过id查询、删除用户以及添加用户 更新UserService：提供通过id查询、删除用户以及添加用户 新建UserController：未完成

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,9 +1,31 @@
 package com.example.demo.service;
 
 import com.example.demo.bean.User;
+import com.example.demo.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class UserService {
-    private User user;
-    //public void creatUser(Integer id, String name, String email, String pwd) {
-     //   User user =
-    //}
+    @Autowired
+    UserMapper userMapper;
+
+    //添加用户
+    public User addUser(Integer id, String name, String organization, String email, Integer paperCount, Integer index, String pwd, String info, Integer role) {
+        User newUser = userMapper.insertUser(new User(id, name, organization, email, paperCount, index, pwd, info, role));
+        return newUser;
+    }
+
+    //用id搜索用户
+    public User getUserById(Integer id) {
+        User newUser = userMapper.getUserById(id);
+        return newUser;
+    }
+
+    //删除用户
+    public int deleteUser(Integer id) {
+        int a = userMapper.deleteUserById(id);
+        return a;
+    }
 }
