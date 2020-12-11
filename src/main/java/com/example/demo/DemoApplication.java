960e7c4a066de936a4533a8edc74dd399462766f
ljm <ljm3307@163.com>
2020-12-11 10:17:22 +0800

    'message'

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,8 +3,10 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@MapperScan(value = "com.example.test.mapper")
+@MapperScan(value = "com.example.demo.mapper")
 @SpringBootApplication
 public class DemoApplication {
 
