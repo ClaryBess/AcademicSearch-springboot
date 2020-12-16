37bacf2029018cf708e583e6ceaab1e7a37b243c
punk-max <54493438+punk-max@users.noreply.github.com>
2020-12-17 01:00:17 +0800

    Update DirectoryService.java

--- src/main/java/com/example/demo/service/DirectoryService.java
+++ src/main/java/com/example/demo/service/DirectoryService.java
@@ -1,43 +1,32 @@
 package com.example.demo.service;
 
-import com.example.demo.bean.Collection;
-import com.example.demo.bean.Directory;
+import com.example.demo.bean.CommonResult;
 import com.example.demo.mapper.DirectoryMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class DirectoryService {
     @Autowired
     DirectoryMapper directoryMapper;
+
     //新建收藏夹
-    public Directory insertDirectory(Directory directory){
-        Directory directory1=directoryMapper.getDirectoryByUserAndName(directory.getUser(),directory.getName());
-        if(directory1!=null)
-            return directory1;
-        directoryMapper.insertDirectory(directory);
-        return directoryMapper.getDirectoryById(directory.getId());
+    public CommonResult CreateDir(String name,Integer user){
+        return new CommonResult(200,"新建成功",directoryMapper.CreateDirectory(user,name));
     }
 
-    //取消关注
-    public int deleteByUserAndName(Integer user, char name){
-        return directoryMapper.deleteDirectoryByUserAndName(user, name);
+    //删除收藏夹
+    public int deleteDir(Integer Did){
+        return directoryMapper.deleteDirectoryById(Did);
     }
 
-    //根据用户和名字获取收藏夹
-    public Directory getDirectoryByUserAndName(Integer user, char name){
-        return directoryMapper.getDirectoryByUserAndName(user, name);
+    //展示用户的所有收藏夹
+    public CommonResult ShowDirByUser(Integer user){
+        return new CommonResult(200,"显示收藏夹",directoryMapper.getDirectoryByUser(user));
     }
 
-    //根据id获取收藏夹
-    public Directory getDirectoryById(Integer id){
-        return directoryMapper.getDirectoryById(id);
-    }
-
-    //获取用户的全部收藏夹
-    public List<Directory> getDirectoryByUser(Integer user){
-        return directoryMapper.getDirectoryByUser(user);
+    //删除收藏夹
+    public int DeleteDirByDid(Integer Did){
+        return directoryMapper.deleteDirectoryById(Did);
     }
 }
