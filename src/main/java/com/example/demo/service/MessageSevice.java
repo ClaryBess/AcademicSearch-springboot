65429a61dc33e601491eedc8b89e61b260fbd852
punk-max <54493438+punk-max@users.noreply.github.com>
2020-12-17 03:28:30 +0800

    Update MessageSevice.java

--- src/main/java/com/example/demo/service/MessageSevice.java
+++ src/main/java/com/example/demo/service/MessageSevice.java
@@ -1,56 +1,54 @@
 package com.example.demo.service;
 
 
-import com.alibaba.druid.sql.visitor.functions.Isnull;
-import com.example.demo.bean.Message;
-import com.example.demo.bean.User;
+import com.example.demo.bean.CommonResult;
 import com.example.demo.mapper.MessageMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-public class MessageSevice {
+public class MessageService {
 
     @Autowired
     MessageMapper messageMapper;
 
-    @Autowired
-    UserService userService;
-
-//    展示学者私信列表，查看发送的消息
-    public List<Message> getMessageByFrom(Integer from){
-        return messageMapper.getMessageByFrom(from);
+    //读私信
+    public CommonResult ReadMessage(Integer id){
+        messageMapper.ReadMessage(id);
+        return new CommonResult(200,"读取成功",messageMapper.ShowMessageDetail(id));
     }
 
-//    展示我的私信列表，查看接受的消息
-    public List<Message> getMessageByTo(Integer to){
-        return messageMapper.getMessageByTo(to);
+    //展示系统消息
+    public CommonResult SysMessage(Integer id){
+        return new CommonResult(200,"系统消息",messageMapper.ShowAllSysMessage(id));
     }
 
-//    展示私信内容
-    public Message getMessageById(Integer id){
-        return messageMapper.getMessageById(id);
+    //展示学者私信
+    public CommonResult ResercherMessage(Integer id){
+        return new CommonResult(200,"学者私信",messageMapper.ShowAllMessageFromRe(id));
     }
 
-
-//    发送私信
-    public int sendMessage(Message message){
-        User fromUser = userService.getUserById((int) message.getFrom());
-        User toUser = userService.getUserById((int) message.getTo());
-        if(fromUser != null && toUser != null){
-            return messageMapper.insertMessage(message);
-        }
-        else{
-            return 0;
-        }
-
+    //展示我的私信
+    public CommonResult MyMessage(Integer id){
+        return new CommonResult(200,"学者私信",messageMapper.ShowMyMessage(id));
     }
 
-//    删除消息
-    public int deleteMessageById(Integer id){
-        return messageMapper.deleteMessageById(id);
+    //删除系统消息或学者私信
+    public CommonResult DeleteOtherMessage(Integer id){
+        int i = messageMapper.DeleteMessageByReceiver1(id);
+        i+= messageMapper.DeleteMessageByReceiver2(id);
+        return new CommonResult(200,"删除成功",i);
+    }
+
+    //删除我的私信
+    public CommonResult DeleteMyMessage(Integer id){
+        int i = messageMapper.DeleteMessageBySender1(id);
+        i+= messageMapper.DeleteMessageBySender2(id);
+        return new CommonResult(200,"删除成功",i);
+    }
+
+    //发送私信
+    public CommonResult SendMessage(Integer from,Integer to,String text){
+        return new CommonResult(200,"发送成功",messageMapper.insertMessage(from,to,text));
     }
 }
-
