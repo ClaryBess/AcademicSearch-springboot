6f3c9fee4120937c628ff1d5c1975f090453f6d1
punk-max <54493438+punk-max@users.noreply.github.com>
2020-12-17 16:23:01 +0800

    Update DirectoryService.java

--- src/main/java/com/example/demo/service/DirectoryService.java
+++ src/main/java/com/example/demo/service/DirectoryService.java
@@ -1,9 +1,11 @@
 package com.example.demo.service;
 
-import com.example.demo.bean.CommonResult;
+import com.example.demo.bean.Directory;
 import com.example.demo.mapper.DirectoryMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DirectoryService {
@@ -11,18 +13,13 @@ public class DirectoryService {
     DirectoryMapper directoryMapper;
 
     //新建收藏夹
-    public CommonResult CreateDir(String name,Integer user){
-        return new CommonResult(200,"新建成功",directoryMapper.CreateDirectory(user,name));
-    }
-
-    //删除收藏夹
-    public int deleteDir(Integer Did){
-        return directoryMapper.deleteDirectoryById(Did);
+    public int CreateDir(String name,Integer user){
+        return directoryMapper.CreateDirectory(user,name);
     }
 
     //展示用户的所有收藏夹
-    public CommonResult ShowDirByUser(Integer user){
-        return new CommonResult(200,"显示收藏夹",directoryMapper.getDirectoryByUser(user));
+    public List<Directory> ShowDirByUser(Integer user){
+        return directoryMapper.getDirectoryByUser(user);
     }
 
     //删除收藏夹
