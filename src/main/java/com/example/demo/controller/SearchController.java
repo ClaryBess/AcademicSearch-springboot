a76266d862ab2ab975d8fff564f44a94ce5ee824
LiShushi <43929922+LiCarry@users.noreply.github.com>
2020-12-20 11:38:24 +0800

    更新SearchController、PaperService、RseearcherService (#6)
    * 更新SearchController、PaperService、RseearcherService
    
    * 修改上次Commit的bug

--- src/main/java/com/example/demo/controller/SearchController.java
+++ src/main/java/com/example/demo/controller/SearchController.java
@@ -15,28 +15,30 @@ public class SearchController {
     @Autowired
     PaperService paperService;
 
+    @Autowired
+    ResearcherService researcherService;
 
-    // 按关键字搜索，模糊搜索
+    // 按关键字搜索，模糊搜索论文
     @RequestMapping("/search/paper/{keyword}")
     public CommonResult searchPaperByTitle(@PathVariable("keyword") String keyword) throws IOException {
         return new CommonResult(200,"success",paperService.getPaperByKeyWord(keyword));
     }
 
-<<<<<<< HEAD
-    @RequestMapping("/search/researcher/{keyword}")
-    public CommonResult SearchResearchByName(@PathVariable("keyword") String keyword){
-        return new CommonResult(200,"success",researcherService.getResearcherByKeyword(keyword));
-=======
-    // 按学科领域搜索
+    // 按学科领域搜索论文
     @RequestMapping("/search/subject/{subject}")
     public CommonResult searchPaperByField(@PathVariable("subject") String subject) throws IOException {
-        return new CommonResult(200, "success", paperService.getPaperByFiled(subject));
->>>>>>> 5a35db3267dafd586f25a7c7822eb6c69913e27f
+        return new CommonResult(200, "success", paperService.getPaperByField(subject));
     }
 
     // 按姓名搜索学者
     @RequestMapping("/search/researcher/{keyword}")
     public CommonResult searchResearcherByName(@PathVariable("keyword") String keyword) throws IOException {
-        return new CommonResult(200, "success", paperService.searchByAuthorId(keyword));
+        return new CommonResult(200, "success", researcherService.getResearcherByKeyword(keyword));
+    }
+
+    // 按时间搜索论文
+    @RequestMapping("/search/{time1}&{time2}")
+    public CommonResult searchPaperByTime(@PathVariable("time1") int start, @PathVariable("time2") int end) throws IOException {
+        return new CommonResult(200, "success", paperService.getPaperByYear(start, end));
     }
 }
