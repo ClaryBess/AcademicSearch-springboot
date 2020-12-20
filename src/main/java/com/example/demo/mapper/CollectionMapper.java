40b5937403d206859664d7b77239a3e97f0c0ede
punk-max <54493438+punk-max@users.noreply.github.com>
2020-12-21 01:02:24 +0800

    Update CollectionMapper.java

--- src/main/java/com/example/demo/mapper/CollectionMapper.java
+++ src/main/java/com/example/demo/mapper/CollectionMapper.java
@@ -15,7 +15,7 @@ public interface CollectionMapper {
 
     //在一个收藏夹里添加一个新的收藏
     @Insert("insert into Collection values(#{directory},#{paper},DEFAULT,#{user})")
-    public int insertCollection(Integer direction, long paper, Integer user);
+    public int insertCollection(Integer directory, long paper, Integer user);
 
     //删除一个收藏
     @Delete("delete from Collection where id=#{id}")
