85ad593ef9f09a27f30049b639d73cd0ec6e8d03
ClaryBess <jexzuboning@126.com>
2020-12-20 23:51:09 +0800

    debug

---
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class AdminController {
+    @Autowired
+    UserService userService;
+
+//    @PostMapping("/admin")
+//    p
+}
