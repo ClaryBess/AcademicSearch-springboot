c1f8cd84399e773e34b3e0d9ae6420d40e78b768
LiShushi <43929922+LiCarry@users.noreply.github.com>
2020-12-21 23:07:19 +0800

    修改SearchController (#7)

--- src/main/java/com/example/demo/controller/SearchController.java
+++ src/main/java/com/example/demo/controller/SearchController.java
@@ -18,27 +18,45 @@ public class SearchController {
     @Autowired
     ResearcherService researcherService;
 
-    // 按关键字搜索，模糊搜索论文
-    @RequestMapping("/search/paper/{keyword}")
-    public CommonResult searchPaperByTitle(@PathVariable("keyword") String keyword) throws IOException {
+    @RequestMapping("/hello")
+    public String sayHello(@RequestParam("hey") String str) {
+        System.out.println("Hello:"+str);
+        return "Hello! Member of group 12!";
+    }
+
+    // 搜索论文
+    @RequestMapping(value = "/search/paper", method = RequestMethod.GET)
+    public CommonResult searchPaperByTitle(@RequestParam("key") String keyword) throws IOException {
         return new CommonResult(200,"success",paperService.getPaperByKeyWord(keyword));
     }
 
     // 按学科领域搜索论文
-    @RequestMapping("/search/subject/{subject}")
-    public CommonResult searchPaperByField(@PathVariable("subject") String subject) throws IOException {
+    @RequestMapping(value = "/search/subject", method = RequestMethod.GET)
+    public CommonResult searchPaperByField(@RequestParam("subject") String subject) throws IOException {
         return new CommonResult(200, "success", paperService.getPaperByField(subject));
     }
 
     // 按姓名搜索学者
-//    @RequestMapping("/search/researcher/{keyword}")
-//    public CommonResult searchResearcherByName(@PathVariable("keyword") String keyword) throws IOException {
-//        return new CommonResult(200, "success", researcherService.getResearcherByKeyword(keyword));
-//    }
+    @RequestMapping(value = "/search/researcher", method = RequestMethod.GET)
+    public CommonResult searchResearcherByName(@RequestParam("key") String keyword) throws IOException {
+        return new CommonResult(200, "success", researcherService.searchResearcherByName(keyword));
+    }
 
     // 按时间搜索论文
-    @RequestMapping("/search/{time1}&{time2}")
-    public CommonResult searchPaperByTime(@PathVariable("time1") int start, @PathVariable("time2") int end) throws IOException {
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public CommonResult searchPaperByTime(@RequestParam(value="time1", required = true) int start, @RequestParam(value="time2", required = true) int end) throws IOException {
         return new CommonResult(200, "success", paperService.getPaperByYear(start, end));
+    }
+
+    // 模糊搜索论文，在标题、摘要和关键字中搜索
+    @RequestMapping(value = "/fuzzysearch", method = RequestMethod.GET)
+    public CommonResult fuzzySearch(@RequestParam(value = "key")String keyword) throws IOException {
+        return new CommonResult(200,"success",paperService.fuzzySearch(keyword));
+    }
+
+    // 按关键字搜索论文
+    @RequestMapping(value = "/search/keyword", method = RequestMethod.GET)
+    public CommonResult searchPaperByTitle(@RequestParam("key") String keyword, @RequestParam("field") String field) throws IOException {
+        return new CommonResult(200,"success",paperService.getPaperByKeyWord(keyword));
     }
 }
